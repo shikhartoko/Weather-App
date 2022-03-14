921bc4c0564479970d15903b354f272603a62362
Shikhar Sharma <shikharsharma@Shikhars-MacBook-Pro.local>
2022-03-14 10:25:56 +0530

    project initialization through xcode

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Shikhar Sharma on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
